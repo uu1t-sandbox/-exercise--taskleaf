ba0074887d49fc5784d8290eb4e9eca9351b2a47
Yuichi Tanikawa <u1.tanikawa@gmail.com>
2018-11-22 23:20:55 +0900

    5-13

--- spec/system/tasks_spec.rb
+++ spec/system/tasks_spec.rb
@@ -4,28 +4,44 @@ describe 'タスク管理機能', type: :system do
   describe '一覧表示機能' do
     let(:user_a) { FactoryBot.create(:user, name: 'ユーザーA', email: 'a@example.com') }
     let(:user_b) { FactoryBot.create(:user, name: 'ユーザーB', email: 'b@example.com') }
+    let!(:task_a) { FactoryBot.create(:task, name: '最初のタスク', user: user_a) }
 
     before do
-      FactoryBot.create(:task, name: '最初のタスク', user: user_a)
       visit login_path
       fill_in 'メールアドレス', with: login_user.email
       fill_in 'パスワード', with: login_user.password
       click_button 'ログインする'
     end
 
-    context 'ユーザーAでログインしているとき' do
-      let(:login_user) { user_a }
+    shared_examples_for 'ユーザーAが作成したタスクが表示される' do
+      it { expect(page).to have_content '最初のタスク' }
+    end
 
-      it 'ユーザーAが作成したタスクが表示される' do
-        expect(page).to have_content '最初のタスク'
+    describe '一覧表示機能' do
+      context 'ユーザーAでログインしているとき' do
+        let(:login_user) { user_a }
+
+        it_behaves_like 'ユーザーAが作成したタスクが表示される'
+      end
+
+      context 'ユーザーBでログインしているとき' do
+        let(:login_user) { user_b }
+
+        it 'ユーザーAが作成したタスクが表示されない' do
+          expect(page).not_to have_content '最初のタスク'
+        end
       end
     end
 
-    context 'ユーザーBでログインしているとき' do
-      let(:login_user) { user_b }
+    describe '詳細表示機能' do
+      context 'ユーザーAでログインしているとき' do
+        let(:login_user) { user_a }
 
-      it 'ユーザーAが作成したタスクが表示されない' do
-        expect(page).not_to have_content '最初のタスク'
+        before do
+          visit task_path(task_a)
+        end
+
+        it_behaves_like 'ユーザーAが作成したタスクが表示される'
       end
     end
   end
